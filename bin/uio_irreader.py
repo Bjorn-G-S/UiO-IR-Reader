ed235841aa2094bf981eb75362eb05461d122d45
nicolhaa <nicolhaa@uio.no>
2021-10-08 09:10:22 +0200

    modified file structure to match py package

---
+++ bin/uio_irreader.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import argparse
+from uio_irreader.reader import IR_Reader, DRIFTS, Transmission
+
+def main():
+    """[summary]
+    """
+
+    ### ADD YOUR PARSER ARGPASE HERE ####
+    parser = argparse.ArgumentParser(prog='uio_irreader', description='Python Opus converter.')
+    args = parser.parse_args()
+
+    ####
+
+    return
+
+
+if __name__ == '__main__':
+    main()
